237cdb2cf1f888a88823fb19c00876f545c1652c
Ralphy Rodriguez <ralmrodriguez018@gmail.com>
2017-12-12 10:31:06 +0800

    first commit.

---
+++ Appwiz/Appwiz/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+using Xamarin.Forms;
+
+namespace Appwiz
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+		}
+
+		protected override void OnStart ()
+		{
+            // Handle when your app starts
+            AppCenter.Start("android=f90978dd-9016-45cb-ad0a-adcaf4383ac2;",
+                   typeof(Analytics), typeof(Crashes));
+        }
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Appwiz/Appwiz/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Appwiz
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+      
+          
+		}
+
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+
+
+            //var result = await DisplayAlert("Confirmation","Do want to proceed?","No","Yes");
+            //if (result.Equals(obj : true))
+            //{
+            //    await Navigation.PushAsync(new SubPage());
+            //}
+            var result = await DisplayAlert("Confirmtion", "Do You Want To Proceed?","Yes","No");
+            if (result.Equals(true))
+            {
+                await Navigation.PushAsync(new SubPage());
+            }
+               
+        }
+
+        //private async void button_OnclickAsync(object sender, EventArgs e)
+        //{
+        //    //Device.BeginInvokeOnMainThread(async () => {
+        //    //    var result = await this.DisplayAlert("Alert!", "Do you really want to continue?", "Yes", "No");
+        //    //    if (result) await Navigation.PushAsync(new Sample()); // or anything else
+        //    //});
+        //    //return true;
+
+        //    var result = await DisplayAlert("title", "Hello", "OK", "Cancel");
+        //    if (result.Equals(true))
+        //    {
+        //        await Navigation.PushAsync(new Sample());
+        //    }
+        //}
+
+    }
+}
